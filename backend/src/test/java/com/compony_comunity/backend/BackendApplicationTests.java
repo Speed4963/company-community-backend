99f175f147ce352b9a982ce7825244853533a80c
ilkdy1234567-ui <ilkdy1234567@gmail.com>
2026-05-15 11:28:27 +0900

    초기파일

---
+++ backend/src/test/java/com/compony_comunity/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compony_comunity.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
